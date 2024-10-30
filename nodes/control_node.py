2cbb3880268bb7c47c1a8b5db350904faddb5234
Mirena Flores <mirena.flores.valdez@gmail.com>
2024-10-30 14:50:06 +0100

    increase steering sensibilitz, removed check for wapoints behind car

--- nodes/control_node.py
+++ nodes/control_node.py
@@ -10,6 +10,7 @@ import math
 from std_msgs.msg import String, Bool, Int64
 from geometry_msgs.msg import PoseStamped
 from mixed_reality.msg import Control, WaypointList, Waypoint, SimPose
+from mixed_reality.for_conversions.for_conversions import For_convertion_utils
 
 
 
@@ -17,10 +18,16 @@ from mixed_reality.msg import Control, WaypointList, Waypoint, SimPose
 #TODO: move this to a config file
 control_uri = "ws://team10.local:8887/wsDrive"
 FIXED_THROTTLE = True
-REAL_CONTROLS = False
-# WAYPOINT_THRESHOLD = 5
-WAYPOINT_THRESHOLD = 0.3*7.33
+REAL_CONTROLS = True
+WAYPOINT_THRESHOLD = 2.5
+# WAYPOINT_THRESHOLD = 0.3*7.33
 ANGLE_THRESHOLD = 0
+
+SIZE_FACTOR=7.33
+X_MAP_SHIFT=48
+Y_MAP_SHIFT=50
+ANGLE_SHIFT=0
+for_conversions = For_convertion_utils(SIZE_FACTOR,X_MAP_SHIFT,Y_MAP_SHIFT,ANGLE_SHIFT)
 
 class Waypoint_control_utils():
         def __init__(self,treshold,angle_treshold):
@@ -67,14 +74,16 @@ class Waypoint_control_utils():
             #print(f"Angle to goal: {angle_y_axis_degrees}")
             angle_difference=self.angle_difference(angle_cur,angle_y_axis_degrees)
             #print(f"angle diff {angle_difference}")
-            steering = (math.radians(angle_difference) / math.pi)*5
+            steering = (math.radians(angle_difference) / math.pi)*10
             throttle=1
+            # print(f"from {[x_cur, y_target]} to {[x_target,y_target]}: steering={steering}")
 
             return steering, throttle, distance, angle_difference
         
         def calculate_distance(self, x_target, y_target, simulator_pose):
             x_cur, _, y_cur = simulator_pose
             distance = math.sqrt((x_target - x_cur)**2 + (y_target - y_cur)**2)
+            print(f"distance between {[round(x_cur,3),round(y_cur,3)]} and {[round(x_target,3),round(y_target,3)]} is {round(distance,3)}")
             return distance
         
         def calculate_angle(self, x_target, y_target, simulator_pose, simulator_orientation):
@@ -332,11 +341,7 @@ current_waypoint_index = 0
 following_waypoints = False
 pub_throttle_steering = None
 
-
-
-
-
-
+TRACKING = True
 
 
 
@@ -363,10 +368,11 @@ def new_going(msg):
     if going and msg.data:
         print(f"CONTROL: State is already driving")
     elif not going and not msg.data:
-        print(f"CONTROL: State is already {state}]")
+        print(f"CONTROL: State is already {state}")
         if pub_throttle_steering is None:
             pub_throttle_steering = rospy.Publisher("control/throttle_steering", Control, queue_size=10)
         pub_throttle_steering.publish(Control(0, steering, True, False, True))
+        state = "stopping"
     elif msg.data:
         state = "driving"
         going = True
@@ -394,30 +400,49 @@ def new_waypoints(msg):
         state = "driving"
         going = True
     print(f"Received waypoint list of length {len(waypoint_list)}\nCurrent waypoint index is {current_waypoint_index}")
-    #print(waypoint_list)
 
-def new_sim_pose(msg):
+# def new_sim_pose(msg):
+#     global sim_pose
+#     global sim_orientation
+
+#     sim_pose = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
+#     quaternion = (
+#         msg.pose.orientation.x,
+#         msg.pose.orientation.y,
+#         msg.pose.orientation.z,
+#         msg.pose.orientation.w)
+#     euler = tf.transformations.euler_from_quaternion(quaternion) #this gives roll, pitch, yaw
+#     angle_rad = euler[1]
+#     if angle_rad<0:
+#         angle_rad = angle_rad + 2*math.pi
+#     sim_orientation = [math.degrees(euler[2]), math.degrees(angle_rad), math.degrees(euler[0])]    #sim_orientation is yaw, pitch, roll
+#     print(f"Angle: {round(sim_orientation[1], 3)}, {round(angle_rad, 3)} rad")
+#     print(f"Quaternion {quaternion}")
+
+def new_pose(msg):
     global sim_pose
     global sim_orientation
-    sim_pose = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
-    quaternion = (
-        msg.pose.orientation.x,
-        msg.pose.orientation.y,
-        msg.pose.orientation.z,
-        msg.pose.orientation.w)
-    euler = tf.transformations.euler_from_quaternion(quaternion) #this gives roll, pitch, yaw
-    angle_rad = euler[1]
-    if angle_rad<0:
-        angle_rad = angle_rad + 2*math.pi
-    sim_orientation = [math.degrees(euler[2]), math.degrees(angle_rad), math.degrees(euler[0])]    #sim_orientation is yaw, pitch, roll
-    print(f"Angle: {round(sim_orientation[1], 3)}, {round(angle_rad, 3)} rad")
-    print(f"Quaternion {quaternion}")
-
-def new_euler(msg):
-    global sim_pose
-    global sim_orientation
-    sim_pose = [msg.x,msg.y,msg.z]
-    sim_orientation = [0,msg.yaw,0]
+    global for_conversions
+    if TRACKING:
+        position = (
+            msg.pose.position.x,
+            msg.pose.position.y,
+            msg.pose.position.z
+        )
+        quaternion = (
+            msg.pose.orientation.x,
+            msg.pose.orientation.y,
+            msg.pose.orientation.z,
+            msg.pose.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        r = math.degrees(euler[2])
+        tracked_pose_sim = for_conversions.real2sim_xyp([position[0], position[1], r])
+        # print(f"Received position is {tracked_pose_sim}")
+        sim_pose = [tracked_pose_sim[0],0,tracked_pose_sim[1]]
+        sim_orientation = [0,tracked_pose_sim[2],0]
+    else:
+        sim_pose = [msg.x,msg.y,msg.z]
+        sim_orientation = [0,msg.yaw,0]
     
     
 
@@ -441,9 +466,10 @@ def control_node():
     global pub_throttle_steering
     global sim_pose
     global sim_orientation
+    global TRACKING
 
     #TODO: get rid of TESTING
-    TESTING = False
+    TESTING = True
     
     rospy.init_node("control_node", anonymous=True)
     rospy.Subscriber("model/throttle_steering", Control, new_throttle_steering)
@@ -451,7 +477,10 @@ def control_node():
     rospy.Subscriber("going", Bool, new_going)
     rospy.Subscriber("waypoints", WaypointList, new_waypoints)
     #rospy.Subscriber("sim/pose", PoseStamped, new_sim_pose)
-    rospy.Subscriber("sim/euler", SimPose, new_euler)
+    if TRACKING:
+        rospy.Subscriber("donkey/pose", PoseStamped, new_pose)
+    else:
+        rospy.Subscriber("sim/euler", SimPose, new_pose)
     if pub_throttle_steering is None:
         pub_throttle_steering = rospy.Publisher("control/throttle_steering", Control, queue_size=10)
     pub_current_waypoint_index = rospy.Publisher("control/current_waypoint_index", Int64, queue_size=10)
@@ -471,28 +500,29 @@ def control_node():
             if following_waypoints and going:
                 #recalculate commands to next waypoint
                 current_waypoint = waypoint_list[current_waypoint_index]
-                print("Going to: ", current_waypoint)
+                # print("Going to: ", current_waypoint)
                 x, y = current_waypoint
 
-                while abs(waypoint_controller.calculate_angle(x,y,sim_pose,sim_orientation))>90:
-                    current_waypoint_index+=1
-                    if current_waypoint_index >=len(waypoint_list):
-                        print("Remaining waypoints are all behind the car")
-                        state = "stopping"
-                        following_waypoints = False
-                        break
-                    current_waypoint = waypoint_list[current_waypoint_index]
-                    x, y = current_waypoint
+                # while abs(waypoint_controller.calculate_angle(x,y,sim_pose,sim_orientation))>90:
+                #     current_waypoint_index+=1
+                #     if current_waypoint_index >=len(waypoint_list):
+                #         print("Remaining waypoints are all behind the car")
+                #         state = "stopping"
+                #         following_waypoints = False
+                #         break
+                #     current_waypoint = waypoint_list[current_waypoint_index]
+                #     x, y = current_waypoint
 
                 print("Going to: ", current_waypoint)
                 x, y = current_waypoint
 
                 distance = waypoint_controller.calculate_distance(x,y,sim_pose)
                 if distance <= WAYPOINT_THRESHOLD:
+                    print("reached")
                     current_waypoint_index += 1  # Move to the next waypoint
                     if  current_waypoint_index < len(waypoint_list):
                         current_waypoint = waypoint_list[current_waypoint_index]
-                        print("Going to: ", current_waypoint)
+                        # print("Going to: ", current_waypoint)
                     else:
                         if TESTING:
                             print("reached final waypoint, looping")
@@ -521,6 +551,7 @@ def control_node():
                     going = False
                     pub_throttle_steering.publish(Control(0, steering, True, False, True))
                 else:
+                    # print(f"Going to {current_waypoint} from {sim_pose}")
                     pub_throttle_steering.publish(Control(throttle, steering, False, throttle<0, not going))
 
             rate.sleep()
